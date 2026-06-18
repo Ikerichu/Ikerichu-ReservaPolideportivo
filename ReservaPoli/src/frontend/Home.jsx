4c949f40fd97d230e2225d505bbf5ef46946dbd6
Iker Moya <92671501+Ikerichu@users.noreply.github.com>
2026-06-18 10:19:58 +0000

    Estructura Layout->Navbar,Main,Footer

---
+++ ReservaPoli/src/frontend/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main className="layout-main">
+      <section id="center">
+        <h1>Get started</h1>
+      </section>
+    </main>
+  )
+}
+
+export default Home
---
+++ ReservaPoli/src/frontend/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__brand">Reserva Poli</div>
+      <nav className="navbar__nav">
+        <a href="#">Inicio</a>
+        <a href="#">Reservas</a>
+        <a href="#">Contacto</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
