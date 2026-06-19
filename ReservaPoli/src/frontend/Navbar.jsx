be0e0ff91d75c14e1a54b96db94cb183c9d6f665
Iker Moya <92671501+Ikerichu@users.noreply.github.com>
2026-06-19 10:23:45 +0000

    Reestructura del nav bar

--- ReservaPoli/src/frontend/Navbar.jsx
+++ ReservaPoli/src/frontend/Navbar.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
+
 function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   return (
     <header className="navbar">
       <div className="navbar__brand">Reserva Poli</div>
       <nav className="navbar__nav">
-        <a href="#">Inicio</a>
-        <a href="#">Reservas</a>
-        <a href="#">Contacto</a>
+        <div className="navbar__left">
+          <a href="#">Inicio</a>
+          <a href="#">Pistas</a>
+          <a href="#">Reservas</a>
+        </div>
+        <div className="navbar__right">
+          {isLoggedIn ? (
+            <a href="#">Perfil</a>
+          ) : (
+            <>
+              <a href="#">Login</a>
+              <a href="#">Registro</a>
+            </>
+          )}
+        </div>
       </nav>
     </header>
   )
